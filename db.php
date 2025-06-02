c8bdddf65e0a60afe045452a0f693e327807f1db
KristinkA6220 <131820620+KristinkA6220@users.noreply.github.com>
2025-06-02 21:13:45 +0300

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $user = 'root';
 $pass = '';
-$dbname = 'korochki';
+$dbname = 'ПОМЕНЯТЬ НАЗВАНИЕ';
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
     die("Ошибка подключения: " . $conn->connect_error);
